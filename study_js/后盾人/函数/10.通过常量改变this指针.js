bd50605316f54e32188f0762e4015bd7ed1c702e
krisfanfan <2755430866@qq.com>
2022-03-01 14:52:06 +0800

    11.箭头函数带来的this变化

--- study_js/后盾人/函数/10.通过常量改变this指针.js
+++ study_js/后盾人/函数/10.通过常量改变this指针.js
@@ -5,7 +5,7 @@ let lesson = {
   lists: ['js', 'css', 'mysql'],
   show: function () {
     const self = this;//通过把this赋值给一个常量，使下面的函数可以通过self得到lesson里面的值
-    return this.lists.map((item) => {
+    return this.lists.map(function(item) {
       console.log(self.site) //通过赋值可以拿到lesson里面的值
       return `${self.site}+${item}`
     })
@@ -18,7 +18,7 @@ let lesson1 = {
   site: '飞飞1',
   lists: ['js', 'css', 'mysql'],
   show: function () {
-    return this.lists.map((item) => {
+    return this.lists.map(function(item) {
       return `${this.site}+${item}`
     }, this) //此this指的是当前lesson1对象,map参数可以传递第二个this参数，把当前作用域的this传递给函数.
   }
