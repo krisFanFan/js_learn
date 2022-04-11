e6cb0f3987775d7e29d1786a0bfa652a3b079205
krisfanfan <2755430866@qq.com>
2022-04-11 10:18:01 +0800

    a

---
+++ study_js/后盾人/2字符串/03字符串替换.js
@@ -0,0 +1,16 @@
+
+const hd = 'houdunren112';
+console.log(hd.replace('houdunren', 'baidu')); //baidu
+// 使用正则
+console.log(hd.replace(/\d/g,'*'))
+// 字符串重复
+console.log('*'.repeat(5));
+
+// 电话号码模糊处理
+function phone(mobile, len = 3) {
+  // console.log(String(mobile).slice(0, 9));
+  // console.log(String(mobile).slice(0, len * -1));
+  return String(mobile).slice(0, len * -1) + '*'.repeat(len);
+}
+console.log(phone(13083771680, 2));
+
